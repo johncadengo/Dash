6c75f2e2f997d32c2b5790bbc97fdbe87e76e04d
John Cadengo <johncadengo@John-Cadengos-MacBook.local>
2011-10-12 16:50:15 -0400

    Initial Commit

---
+++ DashTests/DashTests.h
@@ -0,0 +1,13 @@
+//
+//  DashTests.h
+//  DashTests
+//
+//  Created by John Cadengo on 10/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DashTests : SenTestCase
+
+@end
---
+++ DashTests/DashTests.m
@@ -0,0 +1,32 @@
+//
+//  DashTests.m
+//  DashTests
+//
+//  Created by John Cadengo on 10/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "DashTests.h"
+
+@implementation DashTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in DashTests");
+}
+
+@end
