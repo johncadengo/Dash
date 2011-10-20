fc74cda67b8b3618a35be7acae1c6cdcc162ba8c
John Cadengo <johncadengo@John-Cadengos-MacBook.local>
2011-10-20 18:38:00 -0400

    Beginning to work with a storyboard.

--- Dash/AppDelegate.m
+++ Dash/AppDelegate.m
@@ -11,16 +11,13 @@
 @implementation AppDelegate
 
 @synthesize window = _window;
+
 @synthesize managedObjectContext = __managedObjectContext;
 @synthesize managedObjectModel = __managedObjectModel;
 @synthesize persistentStoreCoordinator = __persistentStoreCoordinator;
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
     return YES;
 }
 
