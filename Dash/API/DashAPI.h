caab421c7a097d4c4b8831960686102e7eb28082
John Cadengo <johncadengo@John-Cadengos-MacBook.local>
2011-10-24 12:25:29 -0400

    Added JSONKit, added compiler flags to it to make sure it is non arc

---
+++ Dash/API/DashAPI.h
@@ -0,0 +1,16 @@
+//
+//  DashAPI.h
+//  Dash
+//
+//  Created by John Cadengo on 10/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "JSONKit.h"
+
+@interface DashAPI : NSObject
+
+@property (nonatomic, strong) JSONDecoder *JSON;
+
+@end
---
+++ Dash/API/DashAPI.m
@@ -0,0 +1,16 @@
+//
+//  DashAPI.m
+//  Dash
+//
+//  Created by John Cadengo on 10/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "DashAPI.h"
+
+@implementation DashAPI
+
+@synthesize JSON;
+
+
+@end
