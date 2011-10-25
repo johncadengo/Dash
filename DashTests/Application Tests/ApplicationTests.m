2ba70f86948ea44d2f9a73139c7bfd74e4f2cc85
John Cadengo <johncadengo@John-Cadengos-MacBook.local>
2011-10-24 22:50:24 -0400

    Adding application test files

---
+++ DashTests/Application Tests/ApplicationTests.m
@@ -0,0 +1,23 @@
+//
+//  ApplicationTests.m
+//  Dash
+//
+//  Created by John Cadengo on 10/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "ApplicationTests.h"
+
+#import <UIKit/UIKit.h>
+//#import "application_headers" as required
+
+@implementation ApplicationTests
+
+// All code under test is in the iOS Application
+- (void)testAppDelegate
+{
+    id yourApplicationDelegate = [[UIApplication sharedApplication] delegate];
+    STAssertNotNil(yourApplicationDelegate, @"UIApplication failed to find the AppDelegate");
+}
+
+@end
