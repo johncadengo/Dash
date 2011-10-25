5e167ffaafc5a003143e746a4d7a591380d5d3b9
John Cadengo <johncadengo@John-Cadengos-MacBook.local>
2011-10-24 22:42:34 -0400

    Adding logic tests

---
+++ DashTests/Logic Tests/PersonLogicTests.m
@@ -0,0 +1,19 @@
+//
+//  PersonLogicTests.m
+//  Dash
+//
+//  Created by John Cadengo on 10/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PersonLogicTests.h"
+
+@implementation PersonLogicTests
+
+// All code under test must be linked into the Unit Test bundle
+- (void)testMath
+{
+    STAssertTrue((1 + 1) == 2, @"Compiler isn't feeling well today :-(");
+}
+
+@end
