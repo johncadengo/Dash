20bed3e57ebc3320cfe3a657cb69463505a5536e
John Cadengo <johncadengo@John-Cadengos-MacBook.local>
2011-10-27 23:46:56 -0400

    FINALLY think I've done it. RESTKIT IS IN.

--- Dash/API/DashAPI.m
+++ Dash/API/DashAPI.m
@@ -7,23 +7,20 @@
 //
 
 #import "DashAPI.h"
-#import "JSONKit.h"
 
 // Private properties
 @interface DashAPI ()
-@property (strong, nonatomic) JSONDecoder *JSON;
+
 @end
 
 @implementation DashAPI
-
-@synthesize JSON = _JSON;
 
 -(id) init 
 {
     self = [super init];
     
     if (self) {
-        self.JSON = [[JSONDecoder alloc] init];
+        return self;
     }
     
     return self;
