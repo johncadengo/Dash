b6a7dd0b570157e1a02359040faf87c7619d38d5
John Cadengo <johncadengo@John-Cadengos-MacBook.local>
2011-11-08 18:28:59 -0500

    Adding the files for our image gallery view controller.

---
+++ Dash/ViewControllers/JCImageGalleryView/JCImageGalleryViewController.h
@@ -0,0 +1,13 @@
+//
+//  JCImageGalleryViewController.h
+//  Dash
+//
+//  Created by John Cadengo on 11/8/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCImageGalleryViewController : UITableViewController
+
+@end
