86f2aa64701d75c979b78c67b5b6c1a743c0fbae
John Cadengo <johncadengo@John-Cadengos-MacBook.local>
2011-11-21 17:10:25 -0500

    Adding a place view cell to display places in the places tab.

---
+++ Dash/ViewControllers/PlacesViewController/PlaceViewCell.h
@@ -0,0 +1,50 @@
+//
+//  PlaceViewCell.h
+//  Dash
+//
+//  Created by John Cadengo on 11/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TISwipeableTableView.h"
+
+typedef enum {
+    PlaceViewCellTypeList = 0,
+    PlaceViewCellTypeSquare = 1
+} PlaceViewCellType;
+
+@class PlaceViewCell;
+@class Place;
+
+@protocol PlaceViewCellDelegate <NSObject>
+- (void)cellBackButtonWasTapped:(PlaceViewCell *)cell;
+@end
+
+@interface PlaceViewCell : TISwipeableTableViewCell
+
+@property (nonatomic, weak) id <PlaceViewCellDelegate> delegate;
+
+// Model
+@property (nonatomic) PlaceViewCellType cellType;
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *address;
+
+// TODO: Some sort of way to indicate which icons will appear
+// and what icons to show: photograph, footprints, medal.
+//@property (nonatomic, strong) 
+
+// Methods
+
++ (CGSize)sizeForPlace:(Place *)place withCellType:(PlaceViewCellType)cellType;
++ (UIFont *)nameFont;
++ (UIFont *)addressFont;
++ (CGSize)textSizeForName:(NSString *)name;
++ (CGSize)textSizeForAddress:(NSString *)address;
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier cellType:(PlaceViewCellType) cellType;
+
+- (void)setCellType:(PlaceViewCellType)cellType;
+- (void)setWithPlace:(Place *)place;
+
+@end
---
+++ Dash/ViewControllers/PlacesViewController/PlaceViewCell.m
@@ -0,0 +1,18 @@
+//
+//  PlaceViewCell.m
+//  Dash
+//
+//  Created by John Cadengo on 11/21/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PlaceViewCell.h"
+
+@implementation PlaceViewCell
+
+@synthesize delegate = _delegate;
+@synthesize name = _name;
+@synthesize address = _address;
+@synthesize cellType = _cellType;
+
+@end
