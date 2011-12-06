24c03af9993c36f739ed9a8dce002e38f9da1ebd
John Cadengo <johncadengo@John-Cadengos-MacBook.local>
2011-12-06 15:29:43 -0500

    Filling out the PlaceSquareViewCell.
    Adding all its instance variables. Figuring out how we're going to store the trophies, badges, etc.

--- Dash/ViewControllers/DashViewController/PlaceSquareViewCell.h
+++ Dash/ViewControllers/DashViewController/PlaceSquareViewCell.h
@@ -8,6 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
+@class Place;
+
 @interface PlaceSquareViewCell : UIView
+
+@property (nonatomic, strong) NSString *name;
+@property (nonatomic, strong) NSString *info;
+@property (nonatomic, strong) NSString *blurb;
+@property (nonatomic, strong) NSMutableArray *badges;
+@property (nonatomic, strong) UIImage *icon;
+
+// TODO: Trophies, images, badges. How will we store them?
+// As distinct UIImages, each one, downloaded from server, stored and cached on phone?
+// Or as predetermined images that are bundled with the product?
+// Or as some hybrid of the two options above?
+
+
+- (void)setWithPlace:(Place *)place;
 
 @end
--- Dash/ViewControllers/DashViewController/PlaceSquareViewCell.m
+++ Dash/ViewControllers/DashViewController/PlaceSquareViewCell.m
@@ -10,6 +10,19 @@
 
 @implementation PlaceSquareViewCell
 
+@synthesize name = _name;
+@synthesize info = _info;
+@synthesize blurb = _blurb;
+@synthesize badges = _badges;
+@synthesize icon = _icon;
+
+#pragma mark - UI Constants
+
+static CGFloat kWidth = 160.0f;
+static CGFloat kHeight = 160.0f;
+
+#pragma mark - Initialization
+
 - (id)initWithFrame:(CGRect)frame
 {
     self = [super initWithFrame:frame];
@@ -18,6 +31,19 @@
     }
     return self;
 }
+
+#pragma mark - Set
+
+- (void)setWithPlace:(Place *)place
+{
+    // Set all our instance variables
+    
+    
+    // Draw self
+    [self setNeedsDisplay];
+}
+
+#pragma mark - Draw
 
 /*
 // Only override drawRect: if you perform custom drawing.
