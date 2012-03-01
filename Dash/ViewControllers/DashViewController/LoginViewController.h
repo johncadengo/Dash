a1bbad4138cc0eb59b79bbc7054bb3a5f523bd61
John Cadengo <johncadengo@gmail.com>
2012-03-01 17:54:01 -0500

    Adding LoginViewController.

---
+++ Dash/ViewControllers/DashViewController/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Dash
+//
+//  Created by John Cadengo on 3/1/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
