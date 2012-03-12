90d6f8abe30c2f6c4cfc181866a44206a71a4f20
John Cadengo <johncadengo@gmail.com>
2012-03-12 14:38:14 -0400

    Adding email/password text fields to welcome screen for creating an account.

--- Dash/ViewControllers/DashViewController/LoginViewController.h
+++ Dash/ViewControllers/DashViewController/LoginViewController.h
@@ -8,14 +8,24 @@
 
 #import <UIKit/UIKit.h>
 
-@interface LoginViewController : UIViewController
+@interface LoginViewController : UIViewController <UITextFieldDelegate>
 
 @property (nonatomic, strong) UILabel *welcome;
 @property (nonatomic, strong) UILabel *introduction;
+@property (nonatomic, strong) UITextField *emailField;
+@property (nonatomic, strong) UITextField *passwordField;
+@property (nonatomic, strong) UIButton *create;
 @property (nonatomic, strong) UIButton *fbconnect;
 @property (nonatomic, strong) UIButton *skip;
 
+/** For dismissing keyboard when view is tapped
+ */
+@property (nonatomic, strong) UITapGestureRecognizer *tap;
 
+- (void)setViewMovedUp:(BOOL)movedUp;
+- (void)loginWithConnect:(id) sender;
+- (void)startDashing:(id) sender;
+- (void)dismissKeyboard:(id) sender;
 - (void)loginWithConnect:(id) sender;
 - (void)showDash:(id) sender;
 
--- Dash/ViewControllers/DashViewController/LoginViewController.m
+++ Dash/ViewControllers/DashViewController/LoginViewController.m
@@ -11,12 +11,19 @@
 #import "Constants.h"
 #import "DashAPI.h"
 
+// For scrolling the textfields up when keyboard is shown
+#define kOFFSET_FOR_KEYBOARD 140.0f
+
 @implementation LoginViewController
 
 @synthesize welcome = _welcome;
 @synthesize introduction = _introduction;
 @synthesize skip = _skip;
 @synthesize fbconnect = _fbconnect;
+@synthesize emailField = _emailField;
+@synthesize passwordField = _passwordField;
+@synthesize create = _create;
+@synthesize tap = _tap;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -33,6 +40,78 @@
     [super didReceiveMemoryWarning];
     
     // Release any cached data, images, etc that aren't in use.
+}
+
+
+#pragma mark - UITextField Delegate
+-(void)textFieldDidBeginEditing:(UITextField *)sender
+{
+    BOOL eitherField = [sender isEqual:self.emailField] || [sender isEqual:self.passwordField];
+    
+    if (eitherField)
+    {
+        //move the main view, so that the keyboard does not hide it.
+        if  (self.view.frame.origin.y >= 0)
+        {
+            [self setViewMovedUp:YES];
+        }
+    }
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
+    if ([textField isEqual:self.emailField]) {
+        // If we are in the email field, we want it to go to the next field, password
+        [self.passwordField becomeFirstResponder];
+    }
+    else {
+        [self dismissKeyboard:textField];
+    }
+    
+    
+    return NO;
+}
+
+//method to move the view up/down whenever the keyboard is shown/dismissed
+-(void)setViewMovedUp:(BOOL)movedUp
+{
+    [UIView beginAnimations:nil context:NULL];
+    [UIView setAnimationDuration:0.25f]; // if you want to slide up the view
+    
+    CGRect rect = self.view.frame;
+    if (movedUp)
+    {
+        // 1. move the view's origin up so that the text field that will be hidden come above the keyboard 
+        // 2. increase the size of the view so that the area behind the keyboard is covered up.
+        rect.origin.y -= kOFFSET_FOR_KEYBOARD;
+        rect.size.height += kOFFSET_FOR_KEYBOARD;
+    }
+    else
+    {
+        // revert back to the normal state.
+        rect.origin.y += kOFFSET_FOR_KEYBOARD;
+        rect.size.height -= kOFFSET_FOR_KEYBOARD;
+    }
+    self.view.frame = rect;
+    
+    [UIView commitAnimations];
+}
+
+
+- (void)keyboardWillShow:(NSNotification *)notif
+{
+    //keyboard will be shown now. depending for which textfield is active, move up or move down the view appropriately
+    
+    BOOL eitherField = [self.emailField isFirstResponder] || [self.passwordField isFirstResponder];
+    
+    if (eitherField && self.view.frame.origin.y >= 0)
+    {
+        [self setViewMovedUp:YES];
+    }
+    else if (!eitherField && self.view.frame.origin.y < 0)
+    {
+        [self setViewMovedUp:NO];
+    }
 }
 
 #pragma mark - View lifecycle
@@ -78,6 +157,41 @@
     self.skip.frame = CGRectMake(20.0f, 390.0f, 280.0f, 40.0f);
     [view addSubview:self.skip];
     
+    // Create account button
+    self.create = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+    [self.create addTarget:self 
+                    action:@selector(loginWithConnect:)
+          forControlEvents:UIControlEventTouchUpInside];
+    [self.create setTitle:@"Create Account" forState:UIControlStateNormal];
+    self.create.frame = CGRectMake(20.0f, 240.0f, 280.0f, 40.0f);
+    [view addSubview:self.create];
+    
+    // Email field
+    self.emailField = [[UITextField alloc] initWithFrame:CGRectMake(20.0f, 140.0f, 280.0f, 40.0f)];
+    self.emailField.borderStyle = UITextBorderStyleRoundedRect;
+    self.emailField.font = [UIFont systemFontOfSize:15];
+    self.emailField.placeholder = @"Email";
+    self.emailField.autocorrectionType = UITextAutocorrectionTypeNo;
+    self.emailField.keyboardType = UIKeyboardTypeDefault;
+    self.emailField.returnKeyType = UIReturnKeyNext;
+    self.emailField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    self.emailField.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;    
+    self.emailField.delegate = self;
+    [view addSubview:self.emailField];
+    
+    // Password field
+    self.passwordField = [[UITextField alloc] initWithFrame:CGRectMake(20.0f, 190.0f, 280.0f, 40.0f)];
+    self.passwordField.borderStyle = UITextBorderStyleRoundedRect;
+    self.passwordField.font = [UIFont systemFontOfSize:15];
+    self.passwordField.placeholder = @"Password";
+    self.passwordField.autocorrectionType = UITextAutocorrectionTypeNo;
+    self.passwordField.keyboardType = UIKeyboardTypeDefault;
+    self.passwordField.returnKeyType = UIReturnKeyDone;
+    self.passwordField.clearButtonMode = UITextFieldViewModeWhileEditing;
+    self.passwordField.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;    
+    self.passwordField.delegate = self;
+    [view addSubview:self.passwordField];
+    
     // Finally, set our self.view
     // NOTE: Do not get self.view in loadView
     self.view = view;
@@ -90,6 +204,10 @@
 {
     [super viewDidLoad];
 
+    self.tap = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                       action:@selector(dismissKeyboard:)];
+    [self.tap setCancelsTouchesInView:NO];
+    [self.view addGestureRecognizer:self.tap];
 }
 
 
@@ -127,6 +245,20 @@
     
     // Goodbye!
     [self dismissModalViewControllerAnimated:YES];
+}
+
+
+- (void)dismissKeyboard:(id) sender
+{
+    // Only dismiss keyboard if it is showing. Otherwise, tap will pass through
+    if ([self.emailField isFirstResponder] || [self.passwordField isFirstResponder]) {
+        // Should find out who is first responder first, but for now just call both text fields
+        [self.emailField resignFirstResponder];
+        [self.passwordField resignFirstResponder];
+        
+        // Scroll our view back down
+        [self setViewMovedUp:NO];
+    }
 }
 
 @end
