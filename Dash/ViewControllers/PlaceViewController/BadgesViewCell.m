9ed7d5a6893600e3382d37fd8f779be1ca2cfda0
John Cadengo <johncadengo@gmail.com>
2012-04-05 15:23:38 -0400

    Badges table view cell.

---
+++ Dash/ViewControllers/PlaceViewController/BadgesViewCell.m
@@ -0,0 +1,29 @@
+//
+//  BadgesViewCell.m
+//  Dash
+//
+//  Created by John Cadengo on 4/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BadgesViewCell.h"
+
+@implementation BadgesViewCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
