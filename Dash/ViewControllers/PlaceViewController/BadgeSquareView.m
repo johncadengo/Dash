44562a5543a0337b702fcee2e5e3f742cfab94e7
John Cadengo <johncadengo@gmail.com>
2012-04-06 16:43:04 -0400

    Adding badge square view.

---
+++ Dash/ViewControllers/PlaceViewController/BadgeSquareView.m
@@ -0,0 +1,31 @@
+//
+//  BadgeSquareView.m
+//  Dash
+//
+//  Created by John Cadengo on 4/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BadgeSquareView.h"
+
+@implementation BadgeSquareView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
