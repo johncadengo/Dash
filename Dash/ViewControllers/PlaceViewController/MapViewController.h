53f212265699f81024bf5fb65e4756a795a28ebc
John Cadengo <johncadengo@gmail.com>
2012-04-30 10:04:00 -0400

    Adding map view controller to display map in app.

---
+++ Dash/ViewControllers/PlaceViewController/MapViewController.h
@@ -0,0 +1,16 @@
+//
+//  MapViewController.h
+//  Dash
+//
+//  Created by John Cadengo on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface MapViewController : UIViewController
+
+@property (nonatomic, strong) MKMapView *map;
+
+@end
---
+++ Dash/ViewControllers/PlaceViewController/MapViewController.m
@@ -0,0 +1,45 @@
+//
+//  MapViewController.m
+//  Dash
+//
+//  Created by John Cadengo on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@interface MapViewController ()
+
+@end
+
+@implementation MapViewController
+
+@synthesize map = _map;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
